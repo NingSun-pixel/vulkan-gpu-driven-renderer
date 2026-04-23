f3863111777239541d266f82cd81d9e6943e532f
NingSun-pixel <ysun4@tcd.ie>
2026-04-23 23:15:19 +0100

    Blend transparent

--- src/vk_engine.cpp
+++ src/vk_engine.cpp
@@ -359,7 +359,7 @@ void VulkanEngine::init_triangle_pipeline()
     //no multisampling
     pipelineBuilder.set_multisampling_none();
     //no blending
-    pipelineBuilder.disable_blending();
+    pipelineBuilder.enable_blending_additive();
     //no depth testing
     pipelineBuilder.disable_depthtest();
 
@@ -429,7 +429,7 @@ void VulkanEngine::init_mesh_pipeline()
     //no multisampling
     pipelineBuilder.set_multisampling_none();
     //no blending
-    pipelineBuilder.disable_blending();
+    pipelineBuilder.enable_blending_additive();
     //no depth testing
     //pipelineBuilder.disable_depthtest();
     pipelineBuilder.enable_depthtest(true, VK_COMPARE_OP_GREATER_OR_EQUAL);
